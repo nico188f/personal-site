18fdb89721a1d240404bdee1f99d28b8be162862
Nicolaj Blach Jensen <nico188f@gmail.com>
2024-10-04 13:48:19 +0200

    Changed logo to a thinner version

--- layouts/HeadDefault.tsx
+++ layouts/HeadDefault.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import logoUrl from "../assets/logo.svg";
+import logoUrl from "../assets/logoThin.svg";
 
 // Default <head> (can be overridden by pages)
 
 export default function HeadDefault() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Demo showcasing Vike" />
-      <link rel="icon" href={logoUrl} />
-    </>
-  );
+   return (
+      <>
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <meta name="description" content="Demo showcasing Vike" />
+         <link rel="icon" href={logoUrl} />
+      </>
+   );
 }
