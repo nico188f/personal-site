b27d7e4df92ab679abd662986de503466ee2beb0
Nicolaj Blach Jensen <nico188f@gmail.com>
2025-05-14 21:02:01 +0200

    Worker inital setup

---
+++ .output/public/_build/assets/client-CLrmJtHB.js
@@ -0,0 +1,1 @@
+import{d as e}from"./client-id8Udq44.js";export{e as default};
--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "@tanstack/react-start/config";
+import { cloudflare } from "unenv";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
@@ -8,5 +9,9 @@ export default defineConfig({
             projects: ["./tsconfig.json"],
          }),
       ],
+   },
+   server: {
+      preset: "cloudflare-module",
+      unenv: cloudflare,
    },
 });
