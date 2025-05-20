7352ab9fdbd4840b89a529a520dacc6d04d27a4d
Nicolaj Blach Jensen <nico188f@gmail.com>
2025-05-20 21:38:10 +0200

    made preview command

--- alchemy.run.ts
+++ alchemy.run.ts
@@ -1,18 +1,27 @@
 import "alchemy/cloudflare";
 import alchemy from "alchemy";
 import { TanStackStart } from "alchemy/cloudflare";
+import { env } from "process";
+
+let stage = "prod";
+let workerSuffix = "";
+
+if (process.argv.includes("--preview")) {
+   stage = "dev";
+   workerSuffix = "-preview";
+}
 
 const app = await alchemy("personal-site", {
-   stage: process.env.USER ?? "dev",
+   stage: process.env.USER ?? stage,
    phase: process.argv.includes("--destroy") ? "destroy" : "up",
    quiet: process.argv.includes("--verbose") ? false : true,
    password: process.env.SECRET_PASSPHRASE,
 });
 
-const website = await TanStackStart("personal-site", {
+const website = await TanStackStart("personal-site" + workerSuffix, {
    command: "bun run build",
    bindings: {
-      GITHUB_TOKEN: alchemy.secret(process.env.GITHUB_TOKEN),
+      GITHUB_TOKEN: alchemy.secret(env.GITHUB_TOKEN),
    },
 });
 
